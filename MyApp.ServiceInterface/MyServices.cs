749c73df6b25fc4c8fec6c36ff4b79ce669a378a
Demis Bellot <demis.bellot@gmail.com>
2017-11-04 06:53:06 -0400

    Update deps

--- MyApp.ServiceInterface/MyServices.cs
+++ MyApp.ServiceInterface/MyServices.cs
@@ -20,7 +20,6 @@ namespace MyApp.ServiceInterface
         //Return index.html for unmatched requests so routing is handled on client
         public object Any(FallbackForClientRoutes request) => 
             new PageResult(Request.GetPage("/"));
-            //new HttpResult(VirtualFileSources.GetFile("index.html")); //Use instead if index.html contains just static HTML
 
         public object Any(Hello request)
         {
