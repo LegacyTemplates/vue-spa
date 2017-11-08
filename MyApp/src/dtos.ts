c876761a28eaa82798d1cb64fc7536041c4ef88f
Demis Bellot <demis.bellot@gmail.com>
2017-11-08 04:49:47 -0500

    Update dtos.ts

--- MyApp/src/dtos.ts
+++ MyApp/src/dtos.ts
@@ -1,25 +1,33 @@
 /* Options:
-Date: 2017-01-01 12:00:00
-Version: 4
+Date: 2017-11-08 04:44:10
+Version: 5.00
 Tip: To override a DTO option, remove "//" prefix before updating
 BaseUrl: http://localhost:5000
 
-//GlobalNamespace:
+//GlobalNamespace: 
 //MakePropertiesOptional: True
 //AddServiceStackTypes: True
 //AddResponseStatus: False
-//AddImplicitVersion:
+//AddImplicitVersion: 
 //AddDescriptionAsComments: True
-//IncludeTypes:
-//ExcludeTypes:
-//DefaultImports:
+//IncludeTypes: 
+//ExcludeTypes: 
+//DefaultImports: 
 */
 
 
-export interface IReturn<T> {
+export interface IReturn<T>
+{
+    createResponse() : T;
 }
 
-export class HelloResponse {
+export interface IReturnVoid
+{
+    createResponse() : void;
+}
+
+export class HelloResponse
+{
     result: string;
 }
 
