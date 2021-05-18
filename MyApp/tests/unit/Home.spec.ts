d4255fbd64f205ffb628c741321026a82529a85d
Darren Reid <darren@reidmail.org>
2021-05-18 16:39:10 +1000

    Update project npm dependencies ready for servicestack/client 1.0.52 update fix for jest virtual dom/node detection issues. Updated Home.spec.ts to use `async` tests, so can `await` for virtual dom.

--- MyApp/tests/unit/Home.spec.ts
+++ MyApp/tests/unit/Home.spec.ts
@@ -1,26 +1,29 @@
-import { shallowMount } from '@vue/test-utils';
-
+import 'node-fetch';
 import { Input } from '@servicestack/vue';
 import Home from '@/components/Home/index.vue';
 import HelloApi from '@/components/Home/HelloApi.vue';
 
+import { shallowMount } from '@vue/test-utils';
+
+
+
 describe('Home.vue', () => {
 
-    it ('should have correct data', () => {
+    it ('should have correct data', async () => {
         const wrapper = shallowMount(HelloApi) as any;
         expect(wrapper.vm.result).toBe('');
     });
 
-    it ('should render correct contents', (done) => {
+    it ('should render correct contents', async (done) => {
         const wrapper = shallowMount(HelloApi, {
             propsData: { name: 'Vue' },
         });
         const vm = wrapper.vm as any;
         expect(vm.name).toBe('Vue');
-        expect(wrapper.find(Input).props().type).toBe('text');
+        expect(wrapper.findComponent(Input).props().type).toBe('text');
         expect(vm.$el.querySelector('h3.result')!.textContent).toBe('');
 
-        vm.result = 'Bye Vue';
+        await wrapper.setData({result: 'Bye Vue'});
 
         expect(vm.$el.querySelector('h3.result')!.textContent).toBe('Bye Vue');
         done();
