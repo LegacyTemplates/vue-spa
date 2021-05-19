8b56dd8e443005a12eafe06bfb71351a2e206788
Darren Reid <darren@reidmail.org>
2021-05-19 15:52:38 +1000

    Mock fetch during test to avoid `UnhandledPromiseRejectionWarning` `DEP0018`.

--- MyApp/tests/setup.js
+++ MyApp/tests/setup.js
@@ -4,3 +4,16 @@ var Controls = require('@servicestack/vue');
 Vue.use(Controls.default);
 
 Vue.config.productionTip = false;
+
+async function mockFetch(url, config) {
+    //ignore fetch when running tests or return custom results based on URL.
+    return {
+        ok: true,
+        status: 200,
+        headers: new Headers(),
+        json: async () => ({}),
+    }
+}
+
+beforeAll(() => jest.spyOn(window, 'fetch'))
+beforeEach(() => window.fetch.mockImplementation(mockFetch))
